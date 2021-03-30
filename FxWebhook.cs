52082d364f7c3fb0da50925327e3ab1a5853b701
Divakar Kumar <kdivakar@pkglobal.com>
2021-03-30 21:55:41 +0530

    Add project files.

---
+++ FxWebhook.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ServerlessWebhooks.Github
+{
+    public static class FxWebhook
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new ContentResult { Content = "<html><body>Hello <b>world</b></body></html>", ContentType = "text/html" };
+
+        }
+    }
+}
+
