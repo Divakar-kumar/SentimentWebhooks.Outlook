33e8cd192d1aa8d2d62dcb0b2ef0e6e09d76f7b6
Divakar Kumar <kdivakar@pkglobal.com>
2021-03-30 23:53:23 +0530

    Add webhook for outlook sentiments

---
+++ Enums/SentimentCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessWebhooks.Github.Enums
+{
+    public enum Reply
+    {
+        ASAP,
+        Later,
+        Normal        
+    }
+}
--- FxWebhook.cs
+++ FxWebhook.cs
@@ -6,19 +6,35 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using ServerlessWebhooks.Github.Enums;
 
 namespace ServerlessWebhooks.Github
 {
     public static class FxWebhook
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        public static async Task<IActionResult> GetOutlookCategory(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            Reply sentimentCategory=Reply.Later;
 
-            return new ContentResult { Content = "<html><body>Hello <b>world</b></body></html>", ContentType = "text/html" };
+            log.LogInformation("Sentiment Webhook http function triggered");
+
+            double sentimentScore = double.Parse(await new StreamReader(req.Body).ReadToEndAsync());
+
+            if (sentimentScore < 0.4)
+            {
+                sentimentCategory = Reply.ASAP;
+            }
+            else if (sentimentScore < 0.7)
+            {
+                sentimentCategory = Reply.Normal;
+            }
+
+            log.LogInformation($"Sentiment category based on score { sentimentCategory }");
+
+            return new OkObjectResult(sentimentCategory);
 
         }
     }
